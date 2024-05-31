47096f01448e71daafd712e699bcd1d847d97462
Tithi Jha <97835872+tithi-jha@users.noreply.github.com>
2024-05-31 23:58:50 +0545

    Time: 6 ms (69.06%), Space: 14.2 MB (21.41%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        for(auto j:mp){
+            if(j.second==1){
+                ans.push_back(j.first);
+            }
+        }
+        return ans;
+    }
+};
